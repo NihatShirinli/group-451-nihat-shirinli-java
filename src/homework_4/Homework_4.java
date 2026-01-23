74197f3e3cb1cda75df20eabf4e3f83a3538827c
NihatShirinli <nihatsirinli2@gmail.com>
2026-01-23 20:18:37 +0400

    Homework_4

---
+++ src/homework_4/Homework_4.java
@@ -0,0 +1,28 @@
+package homework_4;
+
+public class Homework_4 {
+	public static void main(String[] args) {
+//		1 
+		    Student student_1 = new Student("Abubakr", 132, 101);
+			System.out.println("Student's Name: " + student_1.name);
+			System.out.println("Student's Age: " + student_1.age);
+			System.out.println("Student averageGrade: " + student_1.averageGrade);
+		//2
+			int a = 5;
+			int b = 0;
+			int sum = a+b;
+			System.out.println(sum);
+			
+//			int i = 5;
+//			int k = 6;
+//			int j = 7;
+//
+//      double average(int i, int k, double j) {
+//		return 0;
+//	    }
+		//3)
+//			int a = 5;
+//			int b = 0;
+			System.out.println(Math.max(2100, 500, 600, 300));
+      }
+	}
---
+++ src/homework_4/Student.java
@@ -0,0 +1,14 @@
+package homework_4;
+
+public class Student {
+	String name;
+	int age;
+	double averageGrade;
+	
+	public Student(String name, int age, double averageGrade) {
+		this.name = name;
+		this.age = age;
+		this.averageGrade = averageGrade;
+	}
+}
+
